aaf776e3b95822d1160d78e14c3fcd7d5782bdb4
Louis Kowolowski <cubit-minters.0y@icloud.com>
2026-07-04 18:23:30 -0700

    chore: only exists as community

--- lua/plugins/user_none-ls.lua
+++ lua/plugins/user_none-ls.lua
@@ -5,7 +5,6 @@ return {
     local null_ls = require "null-ls"
     opts.sources = require("astrocore").list_insert_unique(opts.sources, {
       null_ls.builtins.formatting.prettier,
-      null_ls.builtins.diagnostics.ruff,
     })
     opts.sources = vim.tbl_filter(function(source)
       return source.name ~= "black"
